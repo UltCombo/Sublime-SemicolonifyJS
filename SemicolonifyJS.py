be60db84493dde2d341ab205a9ecd23a9ed0d8a8
UltCombo <ultcombo@gmail.com>
2014-10-30 10:49:08 -0200

    Update plugin to ST3

--- SemicolonifyJS.py
+++ SemicolonifyJS.py
@@ -7,7 +7,7 @@ import json
 
 package_dir = dirname(realpath(__file__))
 runner_path = join(package_dir, "runner.js")
-p = Popen(["node", runner_path], shell=True, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+p = Popen(["node", runner_path], shell=True, stdout=PIPE, stdin=PIPE, stderr=STDOUT, bufsize=0)
 
 def unload_handler():
 	try:
@@ -18,12 +18,14 @@ def unload_handler():
 class EventHandler(sublime_plugin.EventListener):
 	def on_pre_save(self, view):
 		if splitext(basename(view.settings().get("syntax")))[0] == "JavaScript":
-			content_length = view.size()
-			p.stdin.write((str(content_length) + '\n' + view.substr(sublime.Region(0, content_length))).encode("utf8"))
-			ret = p.stdout.readline().rstrip()
-			if ret:
-				arr = json.loads(ret)
-				edit = view.begin_edit()
-				for pos in reversed(arr):
-					view.insert(edit, view.text_point(pos[0], pos[1]), ";")
-				view.end_edit(edit)
+			view.run_command("insert_semicolons")
+
+class InsertSemicolons(sublime_plugin.TextCommand):
+	def run(self, edit, user_input=None):
+		content_length = self.view.size()
+		p.stdin.write((str(content_length) + '\n' + self.view.substr(sublime.Region(0, content_length))).encode("utf8"))
+		ret = p.stdout.readline().decode("utf8").rstrip()
+		if ret:
+			arr = json.loads(ret)
+			for pos in reversed(arr):
+				self.view.insert(edit, self.view.text_point(pos[0], pos[1]), ";")
